0aa4c60406cfa1acd63494bb17250eb58c40ab6e
marujeyakumar <maru.jeyakumar@mail.utoronto.ca>
2019-09-29 12:09:46 -0400

    Add Material UI and Style Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { styled } from '@material-ui/styles';
+import Button from '@material-ui/core/Button';
+
 import './App.css';
+
+/* Tyring out styled components and material UI */
+const MyButton = styled(Button)({
+  background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+});
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+         Maru Jeyakumar 
         </header>
       </div>
     );
