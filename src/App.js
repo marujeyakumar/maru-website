315f25948c57f595dcc53d270733fc7b098a8b37
marujeyakumar <maru.jeyakumar@mail.utoronto.ca>
2020-03-28 17:44:38 -0400

    add helper template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React, { Component } from 'react';
-import NavBar from './components/NavBar'; 
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Switch} from 'react-router-dom';
+/*------ Pages-----*/
+import {Home} from './Pages/Home';
+import DigitalMarketing from './Pages/Digital-marketing';
+import PaymentProcessing from './Pages/Payment-processing';
+import HRManagement from './Pages/HR-Management';
+import Startup from './Pages/Startup';
+import HomeCRM from './Pages/Home-CRM';
+import About from './Pages/About';
+import Service from './Pages/Service';
+import Process from './Pages/Process';
+import Team from './Pages/Team';
+import Portfolio2col from './Pages/Portfolio-2col';
+import Portfolio3col from './Pages/Portfolio-3col';
+import Portfoliofull4col from './Pages/Portfolio-fullwidth-4col';
+import PortfolioSingle from './Pages/PortfolioSingle';
+import Bloglist from './Pages/Bloglist';
+import BlogSingle from './Pages/BlogSingle';
+import Contact from './Pages/Contact';
+import ScrollToTopRoute from './ScrollToTopRoute';
+import Landing from './Pages/Landing';
 
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-         Maru Jeyakumar 
-         <NavBar />
-        </header>
-        <p> Hi there! I'm Maru. This website is a work in progress. Currently I'm working on building a personal website showcasing 
-          my professional experiences, interests, projects, and my front end developement skills using 
-          React, Material UI and styled components. The website is hosted using AWS Amplify, and you can
-          find the repository on Github, under my username marujeyakumar. 
-        </p>
-      </div>
+class App extends Component{
+    componentDidMount() {
+      this.props.hideLoader();
+  }
+  render(){
+    return(
+        <Router>
+           <Switch>
+             <ScrollToTopRoute exact={true} path={'/'} component={PortfolioSingle} />
+             <ScrollToTopRoute path="/Pages/Digital-marketing" component={DigitalMarketing} />
+             <ScrollToTopRoute path="/Pages/Payment-processing" component={PaymentProcessing} />
+             <ScrollToTopRoute path="/Pages/HR-Management" component={HRManagement} />
+             <ScrollToTopRoute path="/Pages/Startup" component={Startup} />
+             <ScrollToTopRoute path="/Pages/Home-CRM" component={HomeCRM} />
+             <ScrollToTopRoute path="/Pages/About" component={About} />
+             <ScrollToTopRoute path="/Pages/Service" component={Service} />
+             <ScrollToTopRoute path="/Pages/Process" component={Process} />
+             <ScrollToTopRoute path="/Pages/Team" component={Team} />
+             <ScrollToTopRoute path="/Pages/Portfolio-2col" component={Portfolio2col} />
+             <ScrollToTopRoute path="/Pages/Portfolio-3col" component={Portfolio3col} />
+             <ScrollToTopRoute path="/Pages/Portfolio-fullwidth-4col" component={Portfoliofull4col} />
+             <ScrollToTopRoute path="/Pages/PortfolioSingle" component={PortfolioSingle} />
+             <ScrollToTopRoute path="/Pages/Bloglist" component={Bloglist} />
+             <ScrollToTopRoute path="/Pages/BlogSingle" component={BlogSingle} />
+             <ScrollToTopRoute path="/Pages/Contact" component={Contact} />
+             <ScrollToTopRoute path="/Pages/Landing" component={Landing} />
+           </Switch>
+        </Router>
     );
   }
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -4,7 +4,35 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './assets/themify-icon/themify-icons.css';
+import './assets/simple-line-icon/simple-line-icons.css';
+import './assets/elagent/style.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import './assets/animate.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/main.css';
+import './assets/responsive.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+const loader = document.querySelector('#preloader');
+
+// if you want to show the loader when React loads data again
+const showLoader = () => loader.classList.remove('loader--hide');
+
+const hideLoader = () => loader.classList.add('loader--hide');
+
+
+setTimeout(() => 
+  // the show/hide functions are passed as props
+  ReactDOM.render(
+    <App
+      hideLoader={hideLoader}
+      showLoader={showLoader} 
+      />,
+    document.getElementById('root')
+  )
+, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
